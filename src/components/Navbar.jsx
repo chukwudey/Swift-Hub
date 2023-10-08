e9f4173c6e6f910239633a5a5cf40dd0bc60637d
mac <chuks.julian.cj@gmail.com>
2023-10-08 16:53:58 +0100

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className='flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className="w-full text-3xl font-bold text-[#5ae6c8]">
-      SwiftHub.
-    </h1>
-    <ul className='flex hidden'>
-      <li className='p-4'>Home</li>
-      <li className='p-4'>company</li>
-      <li className='p-4'>Resources</li>
-      <li className='p-4'>About</li>
-      <li className='p-4'>Contact</li>
-    </ul>
-    <div>
-      <AiOutlineMenu size={20}/>
-    </div>
-    <div className='fixed left-0 top-0'>
-      <ul>
-      <li className='p-4'>Home</li>
-      <li className='p-4'>company</li>
-      <li className='p-4'>Resources</li>
-      <li className='p-4'>About</li>
-      <li className='p-4'>Contact</li>
+    <div className="flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#5ae6c8]">SwiftHub.</h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
       </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <div
+        className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 block md:hidden"
+            : "fixed left-[-100%] ease-in duration-500"}>
+        <h1 className="w-full text-3xl font-bold m-2 text-[#5ae6c8]">
+          SwiftHub.
+        </h1>
+        <ul className=" p-4 uppercase">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4 border-b border-gray-600">Contact</li>
+        </ul>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
