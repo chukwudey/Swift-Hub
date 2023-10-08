6b93859c3c3d16879c3553da037009a1bbf59f05
mac <chuks.julian.cj@gmail.com>
2023-10-08 07:35:56 +0100

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,25 @@ function Navbar() {
       <h1 className="w-full text-3xl font-bold text-[#5ae6c8]">
       SwiftHub.
     </h1>
-    <ul className='flex'>
+    <ul className='flex hidden'>
       <li className='p-4'>Home</li>
       <li className='p-4'>company</li>
       <li className='p-4'>Resources</li>
       <li className='p-4'>About</li>
       <li className='p-4'>Contact</li>
     </ul>
+    <div>
+      <AiOutlineMenu size={20}/>
+    </div>
+    <div className='fixed left-0 top-0'>
+      <ul>
+      <li className='p-4'>Home</li>
+      <li className='p-4'>company</li>
+      <li className='p-4'>Resources</li>
+      <li className='p-4'>About</li>
+      <li className='p-4'>Contact</li>
+      </ul>
+    </div>
     </div>
   )
 }
